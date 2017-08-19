12348b0673a32c2f71292c520091f9995e8ae957
Adesina <dsnoyin27@gmail.com>
2017-08-19 23:10:40 +0100

    Imported and declared the ngModule used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)] was imported for the ng module used in app.component.ts template
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
