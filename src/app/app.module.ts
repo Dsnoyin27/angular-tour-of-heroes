ca3da696f279fa89ac5a25e73bae7b3ee1272ffe
Adesina <dsnoyin27@gmail.com>
2017-08-21 11:18:35 +0100

    Added heroDetailComponent to the declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule // <-- import the FormsModule before binding with [(ngModel)] was imported for the ng module used in app.component.ts template
   ],
-  declarations: [
-    AppComponent
-  ],
+declarations: [
+  AppComponent,
+  HeroDetailComponent
+],
 
   providers: [],
   bootstrap: [AppComponent]
+
+
 })
 export class AppModule { }
+
